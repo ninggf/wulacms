0b36f97c6586c7a656e5a7945f591272d911ae02
windywany <windywany@163.com>
2017-02-13 18:26:28 +0800

    ready for install

--- conf/install_config.php
+++ conf/install_config.php
@@ -8,6 +8,13 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-//配置要安装的模块
-return ['modules' => ['core', 'dashboard']];
+return [
+	'debug' => env('debug', DEBUG_WARN),
+	'dashboard' => env('dashboard', '{dashboard}'),
+	'url' => env('url', '{url}'),
+	'resource' => [
+		'combinate' => env('resource.combinate', 1),
+		'minify' => env('resource.minify', 1)
+	],
+	'modules' => ['core', 'dashboard']
+];
