e1e80550fae14f0cc7af4dac5c6365a97f656c86
windywany <windywany@163.com>
2019-12-11 11:33:24 +0800

    wulaphp upgrades to v3.4

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 /* 是否开启gzip压缩 */
 define('GZIP_ENABLED', true);
-/* 你可以配置系统的默认模块配置 */
-# define('DEFAULT_MODULE', 'app');
+/* 运行模式 */
+# define('APP_MODE', 'pro');
 /* 如果需要开启URL别名请取消下一行注释，并配置modules/alias.php */
 # define('ALIAS_ENABLED', true);
 /* 如果你的网站以集群的方式提供服务时，请取消下一行的注释，并配置cluster_config.php */
@@ -14,21 +14,24 @@ define('GZIP_ENABLED', true);
 /* 如果你想改变assets目录名，请联消下一行注释并修改其值 */
 # define('ASSETS_DIR', 'assets');
 /* 如果你想改modules目录名，请取消下一行注释并修改其值. */
-# define ('MODULE_DIR', 'modules' );
+# define ('MODULE_DIR', 'modules');
 /* 如果你想改themes目录名，请取消下一行注释并修改其值. */
 # define('THEME_DIR', 'themes');
 /* 如果你想改extensions目录名，请取消下一行注释并修改其值. */
 # define('EXTENSION_DIR', 'extensions');
 /* 如果你想改conf目录名，请取消下一行注释并修改其值. */
-# define ('CONF_DIR', 'conf' );
+# define ('CONF_DIR', 'conf');
 /* 如果你想改libs目录名，请取消下一行注释并修改其值. */
-# define ('LIBS_DIR', 'includes' );
-/* 是否开启防雪崩机制 */
-# define('ANTI_AVALANCHE', true);
-/* 是否开启防CC机制（访问次数/每多少秒），需要在ccredis_config.php配置redis支持 */
-# define('ANTI_CC','10/60');
+# define ('LIBS_DIR', 'includes');
+/* 如果你想改storage目录名，请取消下一行注释并修改其值. */
+# define ('STORAGE_DIR', 'storage');
+/* 如果你想改tmp目录名，请取消下一行注释并修改其值. */
+# define ('TMP_DIR', 'tmp');
+/* 如果你想改logs目录名，请取消下一行注释并修改其值. */
+# define ('LOGS_DIR', 'logs');
 /* 重新定义运行时内存限制 */
-# define ('RUNTIME_MEMORY_LIMIT', '128M' );
+# define ('RUNTIME_MEMORY_LIMIT', '128M');
+/* 如果你要重新定义扩展加载器,请修改 */
 // 以上配置选择性修改
 // //////////////////////////////////////////////////////////////////////////////
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!以下内容不可修改!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -37,14 +40,12 @@ define('GZIP_ENABLED', true);
 define('CONFIG_LOADER_CLASS', 'wula\cms\CmfConfigurationLoader');
 define('MODULE_LOADER_CLASS', 'wula\cms\CmfModuleLoader');
 define('APPROOT', __DIR__ . DIRECTORY_SEPARATOR);
-# define('WULACMF_WEB_INSTALLER', 1);
+define('WULACMF_WEB_INSTALLER', 1);
 defined('PUBLIC_DIR') or define('PUBLIC_DIR', 'wwwroot');
 if (!defined('WWWROOT')) {
     define('WWWROOT', APPROOT . PUBLIC_DIR . DIRECTORY_SEPARATOR);
 }
-if (defined('PHPUNIT_COMPOSER_INSTALL')) {
-    require APPROOT . 'vendor' . DIRECTORY_SEPARATOR . 'wula/wulaphp/bootstrap.php';
-} else {
+if (!defined('PHPUNIT_COMPOSER_INSTALL')) {
     require APPROOT . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 }
 // end of bootstrap.php
